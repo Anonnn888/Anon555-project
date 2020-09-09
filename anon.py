19781e0b864c823c16747119f53271cbba852f31
Anonnn888 <70950457+Anonnn888@users.noreply.github.com>
2020-09-09 20:37:13 +0700

    Add files via upload

---
+++ anon.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+os.system('clear')
+os.system('figlet Tools')
+print "=========================="
+print "Author:Anon555"
+print "Youtube:"
+print "githubb:https://github.com/Anon888"
+print "=========================="
+print
+print "[1] Tools Bajingan"
+print "[2] Perusak"
+print "[3] DDOS"
+print "[4] Coming soon"
+print "[5] spam whatsapp"
+pilih = raw_input('Pilih mana>')
+if pilih == "1":
+	os.system('clear')
+	os.system('figlet enjoyy')
+	os.system('git clone https://github.com/DarknessCyberTeam/BAJINGANv6 ')
+	print "[!]Penginstalan Selesai"
+        print "[!]Lalu Ketik cd BAJINGANv6"
+        print "[!]Lalu sh BAJINGAN.sh"
+        print "[!!] Username:BAJINGAN"
+	print "[!!] Password:Gans"
+elif pilih == "2":
+	os.system('git clone https://github.com//JustAHackers/perusak')
+        os.system('figlet Perusak')
+	print
+        print "ketik cd perusak"
+	print "ketik python2 perusak.py"
+	print "Selesai"
+	print
+elif pilih == "3":
+        os.system('git clone https://github.com/Bl4ckDr460n/Garuda-DDOS')
+	print "[]ketik cd Garuda-DDOS"
+	print "[]ketik python2 garuda.py"
+elif pilih == "4":
+	os.system('figlet aok')
+elif pilih == "5":
+	os.system('git clone https://github.com/sandiwijayani1/SpamWa-1')
+	print "[] ketik cd SpamWa-1"
+	print "[] ketik python3 -m pip install requests"
+	print "[] ketik python3 spam.py"
+
